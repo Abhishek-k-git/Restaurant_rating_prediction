df2e72e223a0e6e9f605a621bad4328246794269
Abhishek kumar <69133983+Abhishek-k-git@users.noreply.github.com>
2021-05-29 09:09:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model=pickle.load(open("model.pkl", 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods = ['POST'])
+@app.route('/predict', methods = ['POST', 'GET'])
 def predict():
     features = [int(x) for x in request.form.values()]
     final_features = [np.array(features)]
